e705d3b6e0a32fad37efd99422993f244d0842f7
Bhagirathsinh8 <bhagirathnakum8@gmail.com>
2025-11-17 20:05:11 +0530

    Frontend Vite Project Setup + Signup Page + Tailwind Css

---
+++ Frontend/src/components/Signup.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Loader2, LoaderCircleIcon } from "lucide-react";
+
+function Signup() {
+  const loading = true;
+  return (
+    <div className="flex items-center w-screen h-screen justify-center">
+      <form
+        action=""
+        className="shadow-lg flex flex-col gap-5 p-5 mt-1 border rounded-lg"
+      >
+        <div className="my-4">
+          {/* <h1 className='text-center'>LOGO</h1> */}
+          <div className="flex items-center justify-center w-full mb-3 p-5 bg-gray-100">
+            <img
+              src="https://img.freepik.com/premium-vector/instagram-vector-social-media-icon_459124-558.jpg?semt=ais_hybrid&w=740&q=80"
+              alt=""
+              height={90}
+              width={90}
+            />
+          </div>
+          <h1 className="text-center font-bold text-2xl mb-3">Signup</h1>
+          <p className="text-center">Signup to see photos and videos from your friends</p>
+        </div>
+
+        <div>
+          <Label>Username</Label>
+          <Input
+            type="text"
+            name="username"
+            className="my-2 focus-visible:ring-transparent"
+          />
+        </div>
+        <div>
+          <Label>Email</Label>
+          <Input
+            type="text"
+            name="username"
+            className="my-2 focus-visible:ring-transparent"
+          />
+        </div>
+        <div>
+          <Label>Password</Label>
+          <Input
+            type="text"
+            name="username"
+            className="my-2 focus-visible:ring-transparent"
+          />
+        </div>
+
+        {loading ? (
+          <Button className="w-full my-4">
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            Please Wait!
+          </Button>
+        ) : (
+          <Button type="submit" className="w-full my-4">
+            Signup
+          </Button>
+        )}
+
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
